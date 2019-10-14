d8d355a93db664f7af5a3a7e663c6c9047e2179c
Rebekah <rebekah.robinson@mail.utoronto.ca>
2019-10-14 00:55:05 -0400

    plz im sad

--- Problems/01-functions-and-variables.js
+++ Problems/01-functions-and-variables.js
@@ -158,13 +158,13 @@ function carefulSubtract (first, second) {
 function typeTester (unknown) {
   // use an if/else construction, a switch/case , or any other branching logic. Remember to
   // return a value. 
-  if (typeof unknown === String) {
+  if (typeof unknown === (String)) {
     return (String + ' yay!');
   } 
-  else if (typeof unknown === Number) {
+  else if (typeof unknown === (Number)) {
     return (Number * Number);
   }
-  else if (typeof unknown === null) { 
+  else if (typeof unknown === (null)) { 
     return 'sorry, I can\'t do anything with a null value.';
 
   } 
