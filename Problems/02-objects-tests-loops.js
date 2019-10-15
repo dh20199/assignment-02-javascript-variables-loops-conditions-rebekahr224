67914e480ac2c270e0c086b279ba4e93a6231b66
Rebekah <rebekah.robinson@mail.utoronto.ca>
2019-10-14 21:10:04 -0400

    GOD SAVE ME

--- Problems/02-objects-tests-loops.js
+++ Problems/02-objects-tests-loops.js
@@ -217,7 +217,7 @@ function prettyIterator (aString, aNumber) {
  * @returns {string} 
  */
 function computeReign (pm) {
-  var p = pm.toparty - pm.from; 
+  var p = (pm.from - pm.toparty); 
   var r = pm.fullName + '\'s' + ' reign was ' + p.toString() + ' ' + 'years long.';
   return r;
 
